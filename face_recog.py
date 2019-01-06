b3d4d377a4b1baf230dfcfbac1571efc0b1c0aa6
Ojaswy Akella <32218681+Ojaswy@users.noreply.github.com>
2019-01-06 19:04:46 +0530

    Update face_recog.py

--- face_recog.py
+++ face_recog.py
@@ -1,32 +1,31 @@
-import cv2
-import sys
+import cv2                                          
+import sys                                          
 
-# Command-line arguments
-imagePath = sys.argv[1]
-cascPath = "haarcascade_frontalface_default.xml"
+# Uncomment the next line to enter image path as a Command-line arguments
+#imagePath = sys.argv[1]
+imagePath = 'coldplay.jpg'                            
+cascPath = "haarcascade_frontalface_default.xml"    
 
-# initialize the haar-cascade
-faceCascade = cv2.CascadeClassifier(cascPath)
+faceCascade = cv2.CascadeClassifier(cascPath)       
 
 # Read the image
-image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image = cv2.imread(imagePath)                      
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)     
 
 # Detect faces in the image
-faces = faceCascade.detectMultiScale(
-    gray,
-    #Location of image from the eye
-    scaleFactor=1.2,
-    #Neighbouring faces - only for accuracy
-    minNeighbors=7,
-    minSize=(30, 30),
-    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+faces = faceCascade.detectMultiScale(               
+    gray,                                           
+    scaleFactor=1.1,                                
+    minNeighbors=3,                                 
+    minSize=(30, 30)                                                                      
 )
-print("Found {0} faces!".format(len(faces)))
 
-# Draw a rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 0), 2)
+print("I have Found {0} faces!".format(len(faces)))        
 
-cv2.imshow("Faces found", image)
-cv2.waitKey(0)
+
+# Draw a rectangle around the faces                 
+for (x, y, w, h) in faces:                                       
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)     
+
+cv2.imshow("Faces found", image)                    
+cv2.waitKey(0)                                      
